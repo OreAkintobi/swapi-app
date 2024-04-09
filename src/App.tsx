a17d02b004eed8a1e8a47b2fdbc0a7ae77b101a7
Ore Akintobi <oreakintobi@gmail.com>
2024-04-09 16:51:14 +0200

    initial test of SWAPI

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import { RootState, decrement, increment } from './store';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Character {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+}
 
 function App() {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
+  const [character, setCharacter] = useState<Character | null>(null);
+
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      try {
+        const response = await axios.get<Character>(
+          'https://swapi.dev/api/people/1/'
+        );
+        setCharacter(response.data);
+      } catch (error) {
+        console.error('Error fetching character:', error);
+      }
+    };
+
+    fetchCharacter();
+  }, []);
 
   return (
     <div className="App">
@@ -28,6 +57,24 @@ function App() {
           </button>
         </div>
       </header>
+
+      <div className="App">
+        <h1>Star Wars Character</h1>
+        {character ? (
+          <div>
+            <h2>{character.name}</h2>
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Hair Color: {character.hair_color}</p>
+            <p>Skin Color: {character.skin_color}</p>
+            <p>Eye Color: {character.eye_color}</p>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Gender: {character.gender}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 }
