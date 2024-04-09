af9f5c5707a8df11744563b0a328227db6993790
Ore Akintobi <oreakintobi@gmail.com>
2024-04-09 16:47:35 +0200

    initial setup of redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { RootState, decrement, increment } from './store';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
       </header>
     </div>
   );
