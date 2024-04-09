f1eec4cee4c47d3182a014f4ad066db5c27b83b2
Ore Akintobi <oreakintobi@gmail.com>
2024-04-09 18:37:06 +0200

    add missing files

---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import { starWarsApi } from './api';
+
+export const store = configureStore({
+  reducer: {
+    [starWarsApi.reducerPath]: starWarsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(starWarsApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
